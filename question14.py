d43d15a2812f95c6f81ff48ee947e9d801e8299d
Burhan <2019cs626@student.uet.edu.pk>
2021-09-25 16:31:55 +0500

    Solved the forteith question

---
+++ question14.py
@@ -0,0 +1,11 @@
+s = "There is 1 2 3 in counting"
+d=l=0
+for c in s:
+    if c.isdigit():
+        d=d+1
+    elif c.isalpha():
+        l=l+1
+    else:
+        pass
+print("Letters", l)
+print("Digits", d)
